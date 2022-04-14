c8b3053cc3831989a1ef53cef4aa114654962f04
murakami <96195951+hiro1103@users.noreply.github.com>
2022-04-14 17:48:03 +0900

    更新しました。

--- data6.py
+++ data6.py
@@ -1,5 +1,3 @@
-from cProfile import label
-from turtle import color
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,6 +10,7 @@ from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import validation_curve
 
 df = pd.read_csv(
     'https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data', header=None)
@@ -90,5 +89,36 @@ plt.xlabel('Number of training examples')
 plt.ylabel('Accuracy')
 plt.legend(loc='lower right')
 plt.ylim([0.8, 1.03])
+plt.tight_layout()
+plt.show()
+
+param_range = [0.001, 0.01, 1.0, 10.0, 100.0]
+# validation_curve関数によりモデルのパラメータを変化させ、交差検証による正解率を算出
+# clf__CはLogisticRegressionオブジェクトのパラメータ
+train_scores, test_scores = validation_curve(estimator=pipe_lr,
+                                             X=X_train, y=y_train,
+                                             param_name='logisticregression__C',
+                                             param_range=param_range, cv=10)
+
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+test_std = np.std(test_scores, axis=1)
+
+plt.plot(param_range, train_mean,
+         color='blue', marker='o', markersize=5,
+         label='Training accuracy')
+plt.fill_between(param_range, train_mean+train_std,
+                 train_mean-train_std, alpha=0.15, color='blue')
+plt.plot(param_range, test_mean, color='green', linestyle='--', marker='s', markersize=5,
+         label='Validation accuracy')
+plt.fill_between(param_range, test_mean+test_std, test_mean -
+                 test_std, alpha=0.15, color='green')
+plt.grid()
+plt.xscale('log')
+plt.legend(loc='lower right')
+plt.xlabel('Parameter C')
+plt.ylabel('Accuracy')
+plt.ylim([0.8, 1.0])
 plt.tight_layout()
 plt.show()
