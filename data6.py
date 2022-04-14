2bdd4e1fe695909086ef75c07bce377279a03119
murakami <96195951+hiro1103@users.noreply.github.com>
2022-04-14 21:26:51 +0900

    21:26

--- data6.py
+++ data6.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,7 +14,13 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import validation_curve
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
-
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score, f1_score
+from sklearn.metrics import make_scorer
+from sklearn.metrics import roc_curve, auc
+from scipy import interp
 df = pd.read_csv(
     'https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data', header=None)
 
@@ -142,3 +149,107 @@ print(gs.best_score_)
 
 # 最良スコアとなるパラメータ値を出力
 print(gs.best_params_)
+
+clf = gs.best_estimator_
+clf.fit(X_train, y_train)
+print('Test accuracy: %.3f' % clf.score(X_test, y_test))
+
+gs = GridSearchCV(estimator=pipe_svc,
+                  param_grid=param_grid,
+                  scoring='accuracy', cv=2)
+scores = cross_val_score(gs, X_train, y_train, scoring='accuracy', cv=5)
+print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+
+# ハイパーパラメータ値として決定木の深さパラメータを指定し、
+# グリットサーチを行うGridSearchCVクラスをインスタンス化
+
+gs = GridSearchCV(estimator=DecisionTreeClassifier(random_state=0),
+                  param_grid=[{'max_depth': [1, 2, 3, 4, 5, 6, 7, None]}],
+                  scoring='accuracy', cv=2)
+
+scores = cross_val_score(gs, X_train, y_train, scoring='accuracy', cv=5)
+print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+
+pipe_svc.fit(X_train, y_train)
+y_pred = pipe_svc.predict(X_test)
+# テストと予測のデータから混同行列を生成
+confmat = confusion_matrix(y_true=y_test, y_pred=y_pred)
+print(confmat)
+
+# 図のサイズを指定
+fig, ax = plt.subplots(figsize=(2.5, 2.5))
+# matshow関数で行列からヒートマップを描画
+ax.matshow(confmat, cmap=plt.cm.Blues, alpha=0.3)
+for i in range(confmat.shape[0]):
+    for j in range(confmat.shape[1]):
+        ax.text(x=j, y=i, s=confmat[i, j], va='center', ha='center')
+
+plt.xlabel('Predicted label')
+plt.ylabel('True label')
+plt.tight_layout()
+plt.show()
+
+print('Precision: %.3f' % precision_score(y_true=y_test, y_pred=y_pred))
+print('Recall: %.3f' % recall_score(y_true=y_test, y_pred=y_pred))
+print('F1: %.3f' % f1_score(y_true=y_test, y_pred=y_pred))
+
+c_gamma_range = [0.01, 0.1, 1.0, 10.0]
+param_grid = [{'svc__C': c_gamma_range, 'svc__kernel': ['linear']},
+              {'svc__C': c_gamma_range, 'svc__gamma': c_gamma_range,
+               'svc__kernel': ['rbf']}]
+scorer = make_scorer(f1_score, pos_label=0)
+gs = GridSearchCV(estimator=pipe_svc,
+                  param_grid=param_grid,
+                  scoring=scorer,
+                  cv=10, n_jobs=-1)
+gs = gs.fit(X_train, y_train)
+print(gs.best_score_)
+print(gs.best_params_)
+
+# スケーリング、主成分分析、ロジスティク回帰を指定して、Pinelineクラスをインスタンス化
+pipe_lr = make_pipeline(StandardScaler(), PCA(n_components=2),
+                        LogisticRegression(penalty='l2', random_state=1,
+                                           solver='lbfgs', C=100.0))
+
+# 2つの特徴量を抽出
+X_train2 = X_train[:, [4, 14]]
+# 層化k分割交差検証イテレーターを表すStratifiedKFoldクラスをインスタンス化
+cv = list(StratifiedKFold(n_splits=3).split(X_train, y_train))
+fig = plt.figure(figsize=(7, 5))
+mean_tpr = 0.0
+# 0から1までの間で100個の要素を生成
+mean_fpr = np.linspace(0, 1, 100)
+all_tpr = []
+for i, (train, test) in enumerate(cv):
+    # predict_probaメソッドで確率を予測、fitメソッドでモデルに適合させる
+    probas = pipe_lr.fit(X_train2[train],
+                         y_train[train]).predict_proba(X_train2[test])
+    # roc_curve関数でROC曲線の性能を計算してプロット
+    fpr, tpr, thresholds = roc_curve(y_train[test], probas[:, 1], pos_label=1)
+    # FPR(X軸)とTPR（Y軸)を線形補間
+    mean_tpr += interp(mean_fpr, fpr, tpr)
+    mean_tpr[0] = 0.0
+    roc_auc = auc(fpr, tpr)
+    plt.plot(fpr, tpr, label='ROC fold %d (area=%0.2f)' % (i+1, roc_auc))
+
+# 当て推量をプロット
+plt.plot([0, 1], [0, 1],
+         linestyle='--', color=(0.6, 0.6, 0.6), label='Random gussing')
+# FPR、TPR、ROC AUCそれぞれの平均を計算してプロット
+mean_tpr /= len(cv)
+mean_tpr[-1] = 1.0
+mean_auc = auc(mean_fpr, mean_tpr)
+plt.plot(mean_fpr, mean_tpr, 'k--',
+         label='Mean ROC (area = %0.2f)' % mean_auc, lw=2)
+
+# 完全に予測が正解したときのROC曲線をプロット
+plt.plot([0, 0, 1], [0, 1, 1],
+         linestyle=':', color='black', label='Perfect performance')
+# グラフの各項目を設定
+plt.xlim([-0.05, 1.05])
+plt.ylim([-0.05, 1.05])
+plt.xlabel('False positive rate')
+plt.ylabel('True positive rate')
+plt.legend(loc='lower right')
+plt.tight_layout()
+plt.show()
