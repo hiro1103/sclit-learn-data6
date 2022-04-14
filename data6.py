ebdbedcce85c5436fd889278fd6bf568852e78f4
murakami <96195951+hiro1103@users.noreply.github.com>
2022-04-14 17:23:32 +0900

    ファイルを更新

--- data6.py
+++ data6.py
@@ -1,5 +1,9 @@
+from cProfile import label
+from turtle import color
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import learning_curve
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -56,3 +60,35 @@ scores = cross_val_score(estimator=pipe_lr,
 
 print('CV accuracy scores: %s' % scores)
 print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+
+pipe_lr = make_pipeline(StandardScaler(), LogisticRegression(penalty='l2', random_state=1,
+                                                             solver='lbfgs', max_iter=10000))
+# learning_curve関数で交差検証による正解率を算出
+train_sizes, train_scores, test_scores = learning_curve(estimator=pipe_lr,
+                                                        X=X_train, y=y_train,
+                                                        train_sizes=np.linspace(0.1, 1.0, 10), cv=10, n_jobs=1)
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+test_std = np.std(test_scores, axis=1)
+plt.plot(train_sizes, train_mean,
+         color='blue', marker='o', markersize=5, label='Training accuracy')
+# fill_between関数で平均標準偏差の幅を塗りつぶす
+# 訓練データのサイズtrain_size、透明度alpha、カラー'blue'を引数に指定
+plt.fill_between(train_sizes, train_mean+train_std, train_mean-train_std,
+                 alpha=0.15, color='blue')
+
+plt.plot(train_sizes, test_mean,
+         color='green', linestyle='--', marker='s', markersize=5,
+         label='Validation accuracy')
+
+plt.fill_between(train_sizes, test_mean + test_std, test_mean-test_std,
+                 alpha=0.15, color='green')
+
+plt.grid()
+plt.xlabel('Number of training examples')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.ylim([0.8, 1.03])
+plt.tight_layout()
+plt.show()
